e7c70d071cebb0153e313d0144cfbfde401c6a11
jmalias <jm.alias@covermanager.com>
2025-04-13 11:26:42 +0200

    Feat: create UserMother.ts

---
+++ test/Units/Users/Domain/Entities/UserMother.ts
@@ -0,0 +1,21 @@
+import { User} from '../../../../../src/Users/Domain/Entities/User';
+
+type UserParams = {
+  id?: string;
+  email?: string;
+  name?: string;
+};
+
+export class UserMother {
+  static random(overrides: UserParams = {}): User {
+    const randomData = {
+      id: 'xxxx-xxxx-xxxx-xxxx',
+      email: 'example@example.com',
+      name: 'John Doe',
+    };
+
+    const finalData = { ...randomData, ...overrides };
+
+    return User.create(finalData.id, finalData.email, finalData.name);
+  }
+}
