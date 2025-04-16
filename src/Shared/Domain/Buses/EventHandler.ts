1a23b6a85e49090eba38fce70d54fcf3cdeb13cb
jmalias <jm.alias@covermanager.com>
2025-04-16 20:52:51 +0200

    Style: lint

--- src/Shared/Domain/Buses/EventHandler.ts
+++ src/Shared/Domain/Buses/EventHandler.ts
@@ -1,6 +1,6 @@
 import { DomainEvent } from '../Events/DomainEvent';
 
 export interface EventHandler {
-  subscribedTo() : Array<DomainEventClass>
+  subscribedTo(): Array<DomainEventClass>;
   handler(event: DomainEvent): Promise<void>;
 }
